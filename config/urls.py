4e9720aa653a04c2dcaca162206853852e4f359e
Paul Muema <pkmuema87@gmail.com>
2026-07-31 02:40:38 +0300

    Refactoring URL configuration and improving indentation in serializers

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('notifications/', include('notifications.urls')),
+    path("api/", include("notifications.urls"))
 ]
--- notifications/serializers.py
+++ notifications/serializers.py
@@ -16,20 +16,20 @@ class BulkNotificationSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=255)
     email = serializers.EmailField()
     notifications = NotificationSerializer(many=True)
-def create(self, validated_data):
-    notifications_data = validated_data.pop("notifications")
+    def create(self, validated_data):
+        notifications_data = validated_data.pop("notifications")
 
-    with transaction.atomic():
-        sender = Sender.objects.create(**validated_data)
+        with transaction.atomic():
+            sender = Sender.objects.create(**validated_data)
 
-        notification_objs = [
-            Notification(sender=sender, **notification)
-            for notification in notifications_data
-        ]
+            notification_objs = [
+                Notification(sender=sender, **notification)
+                for notification in notifications_data
+            ]
 
-        Notification.objects.bulk_create(notification_objs)
+            Notification.objects.bulk_create(notification_objs)
 
-    self.created_count = len(notification_objs)
+        self.created_count = len(notification_objs)
 
-    return sender
-    
+        return sender
+        
