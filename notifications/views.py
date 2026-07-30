edb2cf378c5e79c3fd5f931c778f01dccaa5c892
Paul Muema <pkmuema87@gmail.com>
2026-07-31 02:15:32 +0300

    Created BulkNotificationView to handle notification creation

--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .serializers import BulkNotificationSerializer
+
+
+class BulkNotificationView(APIView):
+    def post(self, request):
+        serializer = BulkNotificationSerializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        sender = serializer.save()
+
+        return Response(
+            {
+                "message": "Notifications created successfully.",
+                "sender_id": sender.id,
+                "notifications_created": serializer.created_count,
+            },
+            status=status.HTTP_201_CREATED,
+        )
